ada5d93be24d1dc87981407ff35741a71074c4a1
Anupam Gogoi <c059237@yara.com>
2024-03-08 14:02:13 -0300

    added

---
+++ share-medicine-sys-app/src/main/java/com/anupam/sharemedicinesysapp/ShareMedicineSysAppApplication.java
@@ -0,0 +1,13 @@
+package com.anupam.sharemedicinesysapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareMedicineSysAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareMedicineSysAppApplication.class, args);
+	}
+
+}
