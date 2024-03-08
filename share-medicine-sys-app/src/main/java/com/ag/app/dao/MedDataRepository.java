8618faa0df2553a310c09dd91e13c371b890c918
Anupam Gogoi <c059237@yara.com>
2024-03-08 14:18:42 -0300

    sasasas

--- share-medicine-sys-app/src/main/java/com/ag/app/dao/MedDataRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ag.app.dao;
-
-import com.ag.app.entity.MedDataEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MedDataRepository extends JpaRepository<MedDataEntity, Integer> {
-}
