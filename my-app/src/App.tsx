70b3146b7c34d34741d9c2ef6f8cab38bb4f1d2e
Anupam Gogoi <c059237@yara.com>
2024-03-24 11:08:17 -0300

    ssassa

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Share from "./components/Share";
 import Home from "./components/Home";
 import Login from "./components/Login";
 function App() {
-  function handleCallBackLogin(data){
-    console.log(data);
+  const [signedIn, setSignedIn] = useState<Boolean>(false);
+  function handleCallBackLogin(data: Boolean) {
+    setSignedIn(data);
   }
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login callBackLogin={handleCallBackLogin}/>} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/share" element={<Share />} />
+        <Route
+          path="/"
+          element={<Login callBackLogin={handleCallBackLogin} />}
+        />
+        {signedIn && <Route path="/home" element={<Home />} />}
+        {signedIn && <Route path="/share" element={<Share />} />}
       </Routes>
     </BrowserRouter>
   );
