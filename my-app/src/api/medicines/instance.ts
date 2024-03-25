bc987fbfd56edac40278b1ebe84e5e9a5928346d
Anupam Gogoi <c059237@yara.com>
2024-03-25 14:35:36 -0300

    sassa

--- my-app/src/api/medicines/instance.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-    baseURL: "https://c219fb60-f3b7-4aca-a7d2-d62a3e1f1a5d-prod.e1-us-east-azure.choreoapis.dev/fwzo/sharemedicineexpapp/endpoint-9090-803/v1.0",
-  });
-  
-export default api;
