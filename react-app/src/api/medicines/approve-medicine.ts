bfb21f9eda272902e76d4bb395a7c3d04cedf0d0
Anupam Gogoi <c059237@yara.com>
2024-03-27 14:09:59 -0300

    sasasas

--- react-app/src/api/medicines/approve-medicine.ts
+++ react-app/src/api/medicines/approve-medicine.ts
@@ -1,14 +1,21 @@
 import api from "./instance";
 import { Medicine } from "../types/medicine";
 
-export async function approveMedicine(accessToken: string, payload: Medicine|null) {
+export async function approveMedicine(
+  accessToken: string,
+  payload: Medicine | null
+) {
   const headers = {
     Authorization: `Bearer ${accessToken}`,
   };
   console.log(payload);
-  const response = await api.post("/approve", payload, {
-    headers: headers
-  });
+  const response = await api
+    .post("/approve", payload, {
+      headers: headers,
+    })
+    .catch((e) => {
+      console.log(e);
+    });
   console.log(response);
   return response;
 }
